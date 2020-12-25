7edc37b84434468311336a0f8ac570740a670927
Brendan O'Neill <brendan.oneill96@hotmail.co.uk>
2020-12-25 13:07:54 +0000

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -131,7 +131,7 @@ label5="$(extend "$(/opt/vc/bin/vcgencmd measure_temp | cut -c "6-9")ºC")"
 label5="$borderBar  $(color $statsLabelColor "Temperature...:") $label5$borderBar"
 
 label6="$(extend "$(curl -s 'https://rss.accuweather.com/rss/liveweather_rss.asp?metric=1&locCode=EN|GB|UK001|PETERBOROUGH|' | sed -n '/Currently:/ s/.*: \(.*\): \([0-9]*\)\([CF]\).*/\2\3, \1/p')")"
-label6="$borderBar  $(color $statsLabelColor "Weather...:") $label6$borderBar"
+label6="$borderBar  $(color $statsLabelColor "Weather.......:") $label6$borderBar"
 
 stats="$label1\n$label2\n$label3\n$label4\n$label5\n$label6"
 
